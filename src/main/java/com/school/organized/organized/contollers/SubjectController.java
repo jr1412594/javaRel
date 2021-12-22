2245cbcdc62db48f1cb98ea7681b927c98ccb3c5
jr1412594 <jr.medina1412@gmail.com>
2021-12-22 10:59:34 -0700

    teachers controller

---
+++ src/main/java/com/school/organized/organized/contollers/SubjectController.java
@@ -0,0 +1,13 @@
+package com.school.organized.organized.contollers;
+
+import com.school.organized.organized.repositories.SubjectRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SubjectController {
+    
+    @Autowired
+    SubjectRepo subjectRepo;
+
+    
+}
--- src/main/java/com/school/organized/organized/contollers/TeacherController.java
+++ src/main/java/com/school/organized/organized/contollers/TeacherController.java
@@ -1,5 +1,35 @@
 package com.school.organized.organized.contollers;
 
+
+import java.util.List;
+
+import com.school.organized.organized.classes.Teacher;
+import com.school.organized.organized.repositories.TeacherRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TeacherController {
+
+    @Autowired
+    TeacherRepo teacherRepo;
     
+    @GetMapping("/teachers")
+    public List<Teacher> getAllTeachers() {
+        return teacherRepo.findAll();
+    }
+
+    @PostMapping("/teachers")
+    public String createTeacher(@RequestBody Teacher teacher) {
+        if(teacher.getName().isEmpty()) {
+            throw new RuntimeException("This teacher is not valid");
+        }
+        teacherRepo.save(teacher);
+        return "Teacher " + teacher.getName() + " has been created.";
+    }
+
 }
